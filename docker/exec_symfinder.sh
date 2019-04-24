844a98dc16da86fcc30bf45bbd207515374be3e2
Johann Mortara <johann.mortara@etu.unice.fr>
2019-04-24 12:18:12 +0200

    Running multiple XPs sequentially

--- docker/exec_symfinder.sh
+++ docker/exec_symfinder.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo "Copying resources to analyse in tmpfs mount..."
-cp -r /resources2/$1 /resources
-java -jar /symfinder.jar resources/$1 $2
+cp -r /resources2/$1 /resources/
+java -jar /symfinder.jar /resources/ $2
---
+++ run_xps.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+
+from mako.template import Template
+
+ENV_FILE_TEMPLATE = r"""
+SOURCES_PACKAGE=${sources_package}
+GRAPH_OUTPUT_PATH=${graph_output_path}
+"""
+
+with open('symfinder.yaml', 'r') as config_file:
+    data = yaml.load(config_file.read(), Loader=yaml.FullLoader)
+    for xp_name, xp_config in data["experiences"].items():
+        for id in xp_config.get("tagIds", []) + xp_config.get("commitIds", []):
+            xp_codename = xp_name + "-" + id
+            with open("symfinder-compose.env", 'w+') as output_file:
+                sources_package = os.path.join(xp_codename, xp_config["sourcePackage"])
+                graph_output_path = "generated_visualizations/data/{}.json".format(xp_codename)
+                output_file.write(Template(ENV_FILE_TEMPLATE).render(
+                    sources_package=sources_package,
+                    graph_output_path=graph_output_path
+                ))
+            os.system("bash rerun.sh %s" % xp_codename)
