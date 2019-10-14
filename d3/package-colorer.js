84958fe6999d0f162f892a15b5765399eba4359a
Johann Mortara <johann.mortara@etu.unice.fr>
2019-10-14 14:15:26 +0200

    Generating a new color for each package entered

--- d3/package-colorer.js
+++ d3/package-colorer.js
@@ -9,7 +9,7 @@ class PackageColorer extends NodesFilter {
         this.packagesMap = new Map();
 
         this.addValue = (n) => {
-            this.packagesMap.set(n, "#0000FF");
+            this.packagesMap.set(n, this.getNewColor().toString());
         };
 
         this.removeValue = (n) => {
@@ -17,9 +17,24 @@ class PackageColorer extends NodesFilter {
         }
     }
 
+    /**
+     * Inspired by https://stackoverflow.com/questions/43193341/how-to-generate-random-pastel-or-brighter-color-in-javascript
+     * @returns {string}
+     */
+    getNewColor() {
+        return "hsl(" + 360 * Math.random() + ',' + 100 + '%,' + 50 + '%)'
+    }
+
     getColorForName(name){
-        var colorFromMap = this.packagesMap.get(name);
-        if(typeof colorFromMap == "undefined"){
+        var colorFromMap = "";
+        var colorFilter = "";
+        this.packagesMap.forEach((value, key) =>{
+            if(name.startsWith(key) && key.length >= colorFilter.length){
+                colorFilter = key;
+                colorFromMap = value;
+            }
+        });
+        if(colorFromMap === ""){
             return "#FF0000";
         } else {
             return colorFromMap.toString();
