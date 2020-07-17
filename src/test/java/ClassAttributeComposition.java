5806be2e39a3806e6a3882605100110558f48c53
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-07-17 16:17:35 +0200

    #22 Ajout de nouveaux tests unitaires pour la composition

--- src/test/java/ClassAttributeComposition.java
+++ src/test/java/ClassAttributeComposition.java
@@ -9,15 +9,61 @@ import static org.junit.Assert.assertEquals;
 public class ClassAttributeComposition extends Neo4jTest{
 
     @Test
-    public void OnePublicMethod() {
+    public void OneAttribute() {
         runTest(graph -> {
             Node rectangleClass = graph.createNode("Rectangle", EntityType.CLASS, EntityVisibility.PUBLIC);
-            Node drawMethod = graph.createNode("draw", EntityType.CLASS, EntityVisibility.PUBLIC);
-            graph.linkTwoNodes(rectangleClass, drawMethod, RelationType.INSTANCIATE);
+            Node drawClass = graph.createNode("draw", EntityType.CLASS, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, drawClass, RelationType.INSTANCIATE);
             graph.setNbComposition();
 
-            assertEquals(1,graph.getNbCompositionRelationship());
             assertEquals(1,graph.getNbAttributeComposeClass());
+        });
+    }
+
+    @Test
+    public void TwoAttributesPerClass() {
+        runTest(graph -> {
+            Node rectangleClass = graph.createNode("Rectangle", EntityType.CLASS, EntityVisibility.PUBLIC);
+            Node drawClass = graph.createNode("draw", EntityType.CLASS, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, drawClass, RelationType.INSTANCIATE);
+
+            Node shapeClass = graph.createNode("Shape", EntityType.CLASS, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, shapeClass, RelationType.INSTANCIATE);
+            graph.setNbComposition();
+
+            assertEquals(2,graph.getNbAttributeComposeClass());
+        });
+    }
+
+    @Test
+    public void MultipleRelations() {
+        runTest(graph -> {
+            Node rectangleClass = graph.createNode("Rectangle", EntityType.CLASS, EntityVisibility.PUBLIC);
+            Node drawClass = graph.createNode("draw", EntityType.CLASS, EntityVisibility.PUBLIC);
+            Node fillClass = graph.createNode("draw", EntityType.CLASS, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, drawClass, RelationType.INSTANCIATE);
+            graph.linkTwoNodes(rectangleClass,fillClass,RelationType.INSTANCIATE);
+
+            Node shapeClass = graph.createNode("Shape", EntityType.CLASS, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, shapeClass, RelationType.EXTENDS);
+            graph.setNbComposition();
+
+            assertEquals(2,graph.getNbAttributeComposeClass());
+        });
+    }
+
+    @Test
+    public void InterfaceAttributeTest() {
+        runTest(graph -> {
+            Node rectangleClass = graph.createNode("Rectangle", EntityType.CLASS, EntityVisibility.PUBLIC);
+            Node drawClass = graph.createNode("draw", EntityType.CLASS, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, drawClass, RelationType.INSTANCIATE);
+
+            Node shapeClass = graph.createNode("Shape", EntityType.INTERFACE, EntityVisibility.PUBLIC);
+            graph.linkTwoNodes(rectangleClass, shapeClass, RelationType.INSTANCIATE);
+            graph.setNbComposition();
+
+            assertEquals(2,graph.getNbAttributeComposeClass());
         });
     }
 }
