f281bdc4ed98bf669b2d7d8d094fe3aaad2c8a61
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-07-20 18:57:00 +0200

    #20 Analyse des paramètres d'une méthode d'une classe

--- src/main/java/visitors/ComposeTypeVisitor.java
+++ src/main/java/visitors/ComposeTypeVisitor.java
@@ -6,9 +6,7 @@ import neograph.NeoGraph;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.*;
 import org.neo4j.driver.types.Node;
 
 import java.util.Optional;
@@ -21,14 +19,12 @@ public class ComposeTypeVisitor extends ImportsVisitor {
 
     private static final Logger logger = LogManager.getLogger(ComposeTypeVisitor.class);
 
-    private ITypeBinding fieldDeclaringClassBinding;
-
     @Override
     public boolean visit(FieldDeclaration field) {
         logger.debug(field);
         ITypeBinding fieldTypeBinding = field.getType().resolveBinding();
         if (field.getParent() instanceof TypeDeclaration && fieldTypeBinding != null) { // prevents the case where the field is an enum, which does not bring variability
-            fieldDeclaringClassBinding = ((TypeDeclaration) field.getParent()).resolveBinding();
+            ITypeBinding fieldDeclaringClassBinding = ((TypeDeclaration) field.getParent()).resolveBinding();
             String parentClassName = fieldDeclaringClassBinding.getQualifiedName();
             Optional<String> classFullName = getClassFullName(fieldTypeBinding);
             if (classFullName.isPresent()) {
@@ -39,7 +35,7 @@ public class ComposeTypeVisitor extends ImportsVisitor {
                     if(!(node.get("name").asString().contains("java") || node.get("name").asString().equals("double") || node.get("name").asString().equals("int")
                         || node.get("name").asString().equals("long") || node.get("name").asString().equals("float") || node.get("name").asString().equals("boolean")
                         || node.get("name").asString().contains("int[]") || node.get("name").asString().contains("double[]") || node.get("name").asString().contains("float[]")
-                        || node.get("name").asString().contains("long[]") || node.get("name").asString().contains("bytes[]") || node.get("name").asString().equals("bytes"))){
+                        || node.get("name").asString().contains("long[]") || node.get("name").asString().contains("bytes[]") || node.get("name").asString().equals("bytes") || node.get("name").asString().equals("byte"))){
                         neoGraph.linkTwoNodes(parentClassNode, node, RelationType.INSTANCIATE);
                         logger.log(Level.getLevel("MY_LEVEL"),"\n ************* Attribute "+ node.get("name") + " ----- " + parentClassNode.get("name") + " ******** \n"  );
                     }
@@ -48,4 +44,35 @@ public class ComposeTypeVisitor extends ImportsVisitor {
         }
         return false;
     }
+
+    @Override
+    public boolean visit(MethodDeclaration method) {
+        ITypeBinding declaringClass;
+        if (!(method.resolveBinding() == null)) {
+            declaringClass = method.resolveBinding().getDeclaringClass();
+            ITypeBinding [] typeparameters = method.resolveBinding().getParameterTypes();
+            String parentClassName = declaringClass.getQualifiedName();
+            Node parentClassNode = neoGraph.getOrCreateNode(parentClassName, declaringClass.isInterface() ? EntityType.INTERFACE : EntityType.CLASS);
+            logger.log(Level.getLevel("MY_LEVEL"),"\nJE SUIS DANS MA FONCTION\n");
+            int size = typeparameters.length;
+            if(size != 0){
+                for (ITypeBinding typeparameter : typeparameters) {
+                    logger.log(Level.getLevel("MY_LEVEL"), "\nJE SUIS DANS MA BOUCLE\n");
+                    //logger.log(Level.getLevel("MY_LEVEL"), "\n******************* type parameter : " + typeparameter.getQualifiedName() + "***************\n");
+                    Optional <Node> typeNode = neoGraph.getNode(typeparameter.getQualifiedName());
+                    typeNode.ifPresent(node -> {
+                        if(!(node.get("name").asString().contains("java") || node.get("name").asString().equals("double") || node.get("name").asString().equals("int")
+                                || node.get("name").asString().equals("long") || node.get("name").asString().equals("float") || node.get("name").asString().equals("boolean")
+                                || node.get("name").asString().contains("int[]") || node.get("name").asString().contains("double[]") || node.get("name").asString().contains("float[]")
+                                || node.get("name").asString().contains("long[]") || node.get("name").asString().contains("bytes[]") || node.get("name").asString().equals("bytes") || node.get("name").asString().equals("byte"))){
+                            neoGraph.linkTwoNodes(parentClassNode, node, RelationType.INSTANCIATE);
+                            logger.log(Level.getLevel("MY_LEVEL"),"\n ************* Attribute "+ node.get("name") + " ----- " + parentClassNode.get("name") + " ******** \n"  );
+                        }
+                    });
+                }
+            }
+        }
+        return false;
+    }
+
 }
