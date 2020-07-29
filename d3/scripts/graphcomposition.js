cacd8cfbc74abcd411d86022f9decf3db37236a6
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-07-29 23:50:23 +0200

    #22 Correction du sens des flèches

--- d3/scripts/graphcomposition.js
+++ d3/scripts/graphcomposition.js
@@ -554,16 +554,16 @@ class Graph {
 
                 this.link
                     .attr("x1", function (d) {
-                        return d.source.x;
-                    })
-                    .attr("y1", function (d) {
-                        return d.source.y;
-                    })
-                    .attr("x2", function (d) {
                         return d.target.x;
                     })
-                    .attr("y2", function (d) {
+                    .attr("y1", function (d) {
                         return d.target.y;
+                    })
+                    .attr("x2", function (d) {
+                        return d.source.x;
+                    })
+                    .attr("y2", function (d) {
+                        return d.source.y;
                     });
 
                 this.label
