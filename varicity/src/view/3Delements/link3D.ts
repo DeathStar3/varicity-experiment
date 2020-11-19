28c2fbe2e28f364680cbcbe966f17e0a9827f107
Florian Focas <florianfocas@gmail.com>
2020-11-19 09:36:10 +0100

    #11 method to change opacity

--- varicity/src/view/3Delements/link3D.ts
+++ varicity/src/view/3Delements/link3D.ts
@@ -21,6 +21,7 @@ export class Link3D {
     render(config: any) {
         this.curve = Curve3.CreateQuadraticBezier(this.src.top, this.src.top.add(new Vector3(0, (this.src.top.y + this.dest.top.y) / 2, 0)), this.dest.top, 25);
         this.line = MeshBuilder.CreateLines("curve", {points: this.curve.getPoints()}, this.scene);
+        this.line.visibility = 0; // defaults at hidden
         if(config.link.colors) {
             for(let c of config.link.colors) {
                 if(c.name == this.type) {
@@ -29,5 +30,9 @@ export class Link3D {
                 }
             }
         }
+    }
+
+    revertOpacity() {
+        this.line.visibility = Math.abs(this.line.visibility - 1);
     }
 }
