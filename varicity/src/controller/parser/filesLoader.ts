e65aa5e1c94ee6d18fe523c10c55ac891a259ced
Florian Focas <florianfocas@gmail.com>
2020-11-23 13:13:06 +0100

    #48 first pass, displays projects on top of browser, can be clicked, not tested for multiple projects but works for one

--- varicity/src/controller/parser/filesLoader.ts
+++ varicity/src/controller/parser/filesLoader.ts
@@ -1,25 +1,36 @@
 export class FilesLoader {
-    private static json = undefined;
+    private static json: Map<string, any> = undefined;
+    // private static json = undefined;
 
-    private static getFileNameOnly(filePath: string) : string {
+    private static getFileNameOnly(filePath: string): string {
         return filePath.split('/').pop().split('.json').shift();
     }
 
-    private static loadJson() : void {
-        const requireContext = require.context('/symfinder_files', false, /\.json$/);
-        FilesLoader.json = {};
+    private static loadJson(): void {
+        const requireContext = require.context('/symfinder_files', false, /^(?!.*-stats\.json)(.*\.json)$/);
+        FilesLoader.json = new Map<string, any>();
         requireContext.keys().forEach((key) => {
             const obj = requireContext(key);
             const simpleKey = FilesLoader.getFileNameOnly(key);
-            FilesLoader.json[simpleKey] = obj;
+            // FilesLoader.json[simpleKey] = obj;
+            FilesLoader.json.set(simpleKey, obj);
         });
         console.log('Loaded json files : ', FilesLoader.json);
     }
 
-    public static loadDataFile(fileName: string) : any {
+    public static loadDataFile(fileName: string): any {
         if (FilesLoader.json === undefined) {
             FilesLoader.loadJson();
         }
-        return FilesLoader.json[fileName];
+        // return FilesLoader.json[fileName];
+        return FilesLoader.json.get(fileName);
+    }
+
+    // returns map keys as string array
+    public static getAllFilenames(): string[] {
+        if(FilesLoader.json === undefined) {
+            FilesLoader.loadJson();
+        }
+        return [...FilesLoader.json.keys()];
     }
 }
--- varicity/src/main.ts
+++ varicity/src/main.ts
@@ -1,7 +1,20 @@
+import { FilesLoader } from './controller/parser/filesLoader';
 import { ClassesPackagesStrategy } from "./controller/parser/strategies/classes_packages.strategy";
 import { SceneRenderer } from "./view/sceneRenderer";
 import {initializeUiComponents} from "./controller/ui/ui";
 
-let entities = new ClassesPackagesStrategy().parse("jfreechart-v1.5.0");
 initializeUiComponents();
-new SceneRenderer(entities);
+let keys = FilesLoader.getAllFilenames();
+console.log(keys);
+var documentProjectArray = document.getElementById("project_selector");
+let scene: SceneRenderer;
+for(let key of keys) {
+    console.log(key);
+    let child = document.createElement("div");
+    child.innerHTML = key;
+    child.addEventListener("click", (ev) => {
+        let entities = new ClassesPackagesStrategy().parse("jfreechart-v1.5.0");
+        scene = new SceneRenderer(entities);
+    });
+    documentProjectArray.appendChild(child);
+}
