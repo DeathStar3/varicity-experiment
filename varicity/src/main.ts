a86e74daaf67026634b6e6be46f07bd2d492f68f
Florian Focas <florianfocas@gmail.com>
2020-11-23 16:22:27 +0100

    #48 can now select one project among many to display

--- varicity/src/main.ts
+++ varicity/src/main.ts
@@ -1,20 +1,18 @@
 import { FilesLoader } from './controller/parser/filesLoader';
 import { ClassesPackagesStrategy } from "./controller/parser/strategies/classes_packages.strategy";
 import { SceneRenderer } from "./view/sceneRenderer";
-import {initializeUiComponents} from "./controller/ui/ui";
+import { initializeUiComponents } from "./controller/ui/ui";
 
 initializeUiComponents();
 let keys = FilesLoader.getAllFilenames();
-console.log(keys);
 var documentProjectArray = document.getElementById("project_selector");
-let scene: SceneRenderer;
-for(let key of keys) {
-    console.log(key);
+let scene: SceneRenderer = new SceneRenderer();
+for (let key of keys) {
     let child = document.createElement("div");
     child.innerHTML = key;
     child.addEventListener("click", (ev) => {
-        let entities = new ClassesPackagesStrategy().parse("jfreechart-v1.5.0");
-        scene = new SceneRenderer(entities);
+        let entities = new ClassesPackagesStrategy().parse(key);
+        scene.buildScene(entities);
     });
     documentProjectArray.appendChild(child);
 }
--- varicity/src/view/sceneRenderer.ts
+++ varicity/src/view/sceneRenderer.ts
@@ -7,106 +7,71 @@ import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight } from "@baby
 import { ConfigLoader } from '../controller/parser/configLoader';
 
 export class SceneRenderer {
-    constructor(entitiesList: EntitiesList) {
+
+    scene: Scene;
+    engine: Engine;
+    config: any;
+    camera: ArcRotateCamera;
+    light: HemisphericLight;
+
+    canvas: HTMLCanvasElement;
+
+    constructor() {
         // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+        this.canvas = document.createElement("canvas");
+        this.canvas.style.width = "100%";
+        this.canvas.style.height = "100%";
+        this.canvas.id = "gameCanvas";
+        document.body.appendChild(this.canvas);
 
         // initialize babylon scene and engine
-        var engine: Engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+        this.engine = new Engine(this.canvas, true);
+        this.scene = new Scene(this.engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", 2 * Math.PI / 3, Math.PI / 3, 1000, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        camera.panningSensibility = 10;
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
-        // var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-        // sphere.setPositionWithLocalVector(new Vector3(0, 0, 0));
-        // var sphere2: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
-        // sphere2.setPositionWithLocalVector(new Vector3(3, 0, 0));
+        this.camera = new ArcRotateCamera("Camera", 2 * Math.PI / 3, Math.PI / 3, 1000, Vector3.Zero(), this.scene);
+        this.camera.attachControl(this.canvas, true);
+        this.camera.panningSensibility = 10;
+        this.light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
 
-        // let districtElement = new PackageImplem("com");
-        // districtElement.addDistrict(new PackageImplem("com.polytech"));
-        // districtElement.districts[0].addBuilding(new ClassImplem("class1", 10, 15));
-        // districtElement.districts[0].addBuilding(new ClassImplem("class2", 10, 15));
-        // districtElement.districts[0].addBuilding(new ClassImplem("class3", 10, 15));
-        // let districtElement2 = new PackageImplem("com");
-        // districtElement2.addDistrict(new PackageImplem("com.polytech"));
-        // districtElement2.districts[0].addBuilding(new ClassImplem("class1", 10, 15));
-        // districtElement2.districts[0].addBuilding(new ClassImplem("class2", 10, 15));
-        // districtElement2.districts[0].addBuilding(new ClassImplem("class3", 10, 15));
+        this.config = ConfigLoader.loadDataFile("config");
 
-        // let entities: PackageImplem[] = [];
-        // entities.push(districtElement, districtElement2);
-
-        const config = ConfigLoader.loadDataFile("config");
-        
-        const city = new City3D(config, scene, entitiesList);
-        city.build();
-        city.place();
-        city.render();
-        // let nextX = 0
-        // // entities.forEach(d => {
-        // entitiesList.districts.forEach(d => {
-        //     let d3elem = new District3D(scene, d, 0, nextX - (d.getTotalWidth() / 2), 0);
-        //     d3elem.build();
-        //     d3elem.render(config);
-        //     nextX += d3elem.elementModel.getTotalWidth() + 5; // 10 = padding between districts
-        // });
-
-        // let d3elem = new District3D(scene, districtElement, 0);
-        // d3elem.render();
-        // let d3elem2 = new District3D(scene, districtElement, 0);
-        // d3elem2.render();
-        // let d3Building = new Building3D(scene, new ClassImplem("classBuilding",10,10),100,10,10);
-        // d3Building.render();
-
-        // var quartier: Mesh = MeshBuilder.CreateBox("package", {height: 20, width: 270, depth: 200}, scene);
-        // quartier.setPositionWithLocalVector(new Vector3(0 + (270 /2), -20, 0));
-        // var quartier2: Mesh = MeshBuilder.CreateBox("package", {height: 20, width: 10, depth: 10}, scene);
-        // quartier2.setAbsolutePosition(new Vector3(0, 20, 0));
-        // for(let i =0; i<10; i++) {
-        //     var building: Mesh = MeshBuilder.CreateBox("buildin"+i, { height: 20, width: 10, depth: 10 }, scene);
-        //     building.setPositionWithLocalVector(new Vector3(30* i, 0, 0));
-        //     building.renderOutline = true;
-        //     building.outlineColor = new Color3(0, 1, 0);
-        //     building.outlineWidth = 0.1;
-        //     building.edgesColor = new Color4(1, 0, 0);
-        //     var mat = new StandardMaterial("buildingMat"+i, scene);
-        //     mat.ambientColor = new Color3(1, 0, 0);
-        //     mat.diffuseColor = new Color3(1, 0, 0);
-        //     mat.emissiveColor = new Color3(1, 0, 0);
-        //     mat.specularColor = new Color3(1, 0, 0);
-        //     building.material = mat;
-        //     var curve = Curve3.CreateQuadraticBezier(new Vector3(30*i, 20, 10), new Vector3(30*i+15, 35, 10), new Vector3(30*(i+1), 20, 10), 25);
-        //     MeshBuilder.CreateLines("curve"+i, {points: curve.getPoints()}, scene);
-        // }
-
-        document.getElementById("reset_camera").addEventListener("click", (ev) => {
-            camera.position = Vector3.Zero();
-            camera.radius = 1000;
-            camera.alpha =  2 * Math.PI / 3;
-            camera.beta = Math.PI / 3;
+        document.getElementById("reset_camera").addEventListener("click", () => {
+            this.camera.position = Vector3.Zero();
+            this.camera.radius = 1000;
+            this.camera.alpha = 2 * Math.PI / 3;
+            this.camera.beta = Math.PI / 3;
         });
 
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
             if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+                if (this.scene.debugLayer.isVisible()) {
+                    this.scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                    this.scene.debugLayer.show();
                 }
             }
         });
 
         // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
         });
+    }
+
+    buildScene(entitiesList: EntitiesList) {
+        this.scene.dispose();
+        this.scene = new Scene(this.engine);
+
+        this.camera = new ArcRotateCamera("Camera", 2 * Math.PI / 3, Math.PI / 3, 1000, Vector3.Zero(), this.scene);
+        this.camera.attachControl(this.canvas, true);
+        this.camera.panningSensibility = 10;
+        this.light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
+        
+        const city = new City3D(this.config, this.scene, entitiesList);
+        city.build();
+        city.place();
+        city.render();
     }
 }
