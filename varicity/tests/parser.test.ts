c063b5ae22bd9d31bf8ed8d86814e603bb2f29b1
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-11-26 03:22:27 +0100

    #41 CI fixing-5

--- varicity/tests/parser.test.ts
+++ varicity/tests/parser.test.ts
@@ -1,8 +1,18 @@
 import {expect} from 'chai';
 
+import { ClassesPackagesStrategy } from "../src/controller/parser/strategies/classes_packages.strategy";
+
 describe('calculate', function() {
     it('add', function() {
       let result = 5+ 2;
       expect(result).equal(7);
     }); 
   });
+
+describe('parsing', function() {
+  it('parse', function() {
+    let entities = new ClassesPackagesStrategy().parse('test1WithoutLinks');
+    console.log(entities);
+    expect(7).equal(7);
+  }); 
+});
