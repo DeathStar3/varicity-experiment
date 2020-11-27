1c803797f4dd47dc04d9c9a7c6f5394d09020449
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-11-27 17:25:29 +0100

    #41 Add prepare to merge

--- varicity/tests/parser.test.ts
+++ varicity/tests/parser.test.ts
@@ -2,13 +2,6 @@ import {expect} from 'chai';
 
 import { ClassesPackagesStrategy } from "../src/controller/parser/strategies/classes_packages.strategy";
 
-describe('calculate', function() {
-    it('add', function() {
-      let result = 5+ 2;
-      expect(result).equal(7);
-    }); 
-  });
-
 describe('parsing without links', function() {
   it('parse', function() {
     let entities = new ClassesPackagesStrategy().parse('test1WithoutLinks');
--- varicity/webpack.config.js
+++ varicity/webpack.config.js
@@ -1,8 +1,12 @@
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
     entry: {
-      'main':'./src/main.ts',
+      'main': path.resolve(appDirectory, "src/main.ts"),
       'parserTest':'./tests/parser.test.ts'
     },
     output: {
@@ -11,6 +15,14 @@ module.exports = {
     },
     resolve: {
       extensions: ['.ts', '.js', '.tsx', '.jsx']
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080, //port that we're using for local host (localhost:8080)
+        disableHostCheck: true,
+        contentBase: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        publicPath: '/',
+        hot: true
     },
     module: {
       rules: [
@@ -27,5 +39,12 @@ module.exports = {
         }
       ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html")
+        }),
+        new CleanWebpackPlugin()
+    ],
     mode: "development"
   }
