9b3e643184e707295be1e011188cd1e1f60d1344
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-11-30 17:05:43 +0100

    #61 new tests in integration tests

--- docker/varicity_integration_tests/files/tests/parserClassesPackages.test.ts
+++ docker/varicity_integration_tests/files/tests/parserClassesPackages.test.ts
@@ -11,4 +11,36 @@ describe('parsing all tests projects with classe packages strategy', function() 
     let numberOfLinks = entities.links.length;
     expect(numberOfLinks).equal(3);
   }); 
+
+  it('parse abactract decorator', function() {
+    let entities = new ClassesPackagesStrategy().parse('composition_levels_inheritance');
+    let buildings = entities.buildings
+    let numberOfBuiildings = buildings.length;
+    expect(numberOfBuiildings).equal(1);
+    let numberOfLinks = entities.links.length;
+    expect(numberOfLinks).equal(0);
+  }); 
+
+  it('parse composition_levels_mixed', function() {
+    let entities = new ClassesPackagesStrategy().parse('composition_levels_mixed');
+    let buildings = entities.buildings
+    let numberOfBuiildings = buildings.length;
+    expect(numberOfBuiildings).equal(1);
+    let numberOfLinks = entities.links.length;
+    expect(numberOfLinks).equal(0);
+  }); 
+
+  it('parse decorator', function() {
+    let entities = new ClassesPackagesStrategy().parse('decorator');
+    let districts = entities.districts[0].districts[0].districts
+    let numberOfDistricts = districts.length;
+    expect(numberOfDistricts).equal(1);
+    let numberOfBuiildings = 0;
+    districts.forEach(d => {
+      numberOfBuiildings += d.buildings.length
+    })
+    expect(numberOfBuiildings).equal(4);
+    let numberOfLinks = entities.links.length;
+    expect(numberOfLinks).equal(3);
+  }); 
 });
