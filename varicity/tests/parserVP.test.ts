c3eade0788387afa3319916234464c6629276b5f
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-12-03 15:07:18 +0100

    #61 finalize unit test for vp parser

--- varicity/tests/parserVP.test.ts
+++ varicity/tests/parserVP.test.ts
@@ -1,27 +1,34 @@
 import {expect} from 'chai';
 
 import { VPVariantsStrategy } from "../src/controller/parser/strategies/vp_variants.strategy";
+import { District } from '../src/model/entities/district.interface';
 
+function countBuilding(districts: District[]) : number{
+  let sum = 0;
+  districts.forEach(d => {
+    sum += d.buildings.length;
+    sum += countBuilding(d.districts)
+  })
+   return sum;
+}
+
+function countDistricts(districts: District[]) : number{
+  let sum = 0;
+  districts.forEach(d => {
+    sum += 1;
+    sum += countBuilding(d.districts)
+  })
+   return sum;
+}
 describe('parsing without links', function() {
   it('parse', function() {
     let entities = new VPVariantsStrategy().parse('test3ForVPParser');
-    // console.log(entities.districts[0]);
+    // console.log(entities.districts);
+    // console.log(entities.districts[0].districts[0].districts);
     let dis = entities.districts[0].districts
-    let numberOfBuiildings = 0;
-    let numberOfDistricts = 0;
-    // console.log(dis[0].vp);
-    dis.forEach(d => {
-      // console.log(d);
-      // console.log(d.vp)
-      numberOfBuiildings += d.buildings.length;
-      // if(d.vp !== undefined){
-      //   numberOfBuiildings += 1;
-      // }
-      numberOfDistricts += d.districts.length;
-      numberOfBuiildings += numberOfDistricts;
-    })
-    console.log(numberOfBuiildings);
-    console.log(numberOfDistricts);
-    // expect(numberOfBuiildings).equal(5);
+    let numberOfDistricts = countDistricts(entities.districts[0].districts);
+    let numberOfBuiildings = countBuilding(entities.districts[0].districts) + countDistricts(entities.districts[0].districts)
+    expect(numberOfBuiildings).equal(4);
+    expect(numberOfDistricts).equal(2);
   }); 
 });
