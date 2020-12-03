f9428ba325c77e876b52b5a68499f18b0f4b9b99
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2020-12-03 10:14:18 +0100

    #61 correct vp parser test

--- varicity/tests/parserVP.test.ts
+++ varicity/tests/parserVP.test.ts
@@ -5,14 +5,23 @@ import { VPVariantsStrategy } from "../src/controller/parser/strategies/vp_varia
 describe('parsing without links', function() {
   it('parse', function() {
     let entities = new VPVariantsStrategy().parse('test3ForVPParser');
-    console.log(entities.districts[0].districts[0]);
-    let districts = entities.districts[0].districts[0].districts
-    let numberOfDistricts = districts.length;
-    // expect(numberOfDistricts).equal(0);
+    // console.log(entities.districts[0]);
+    let dis = entities.districts[0].districts
     let numberOfBuiildings = 0;
-    districts.forEach(d => {
-      numberOfBuiildings += d.buildings.length
+    let numberOfDistricts = 0;
+    // console.log(dis[0].vp);
+    dis.forEach(d => {
+      // console.log(d);
+      // console.log(d.vp)
+      numberOfBuiildings += d.buildings.length;
+      // if(d.vp !== undefined){
+      //   numberOfBuiildings += 1;
+      // }
+      numberOfDistricts += d.districts.length;
+      numberOfBuiildings += numberOfDistricts;
     })
+    console.log(numberOfBuiildings);
+    console.log(numberOfDistricts);
     // expect(numberOfBuiildings).equal(5);
   }); 
 });
